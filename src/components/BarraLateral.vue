ab84bd7b793e65a6419e1e21c47410544f33194d
yuri <yuuriresende@hotmail.com>
2024-02-29 00:09:43 -0300

    code ident

--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -1,5 +1,5 @@
 <template>
- <header>
+  <header>
     <h1>
       <img src="../assets/logo.png" alt="">
     </h1>
@@ -24,34 +24,34 @@
         </li>
       </ul>
     </nav>
- </header>
+  </header>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-    name: 'BarraLateral',
-    emits: ['alterarTema'],
-    data(){
-      return {
-        modoEscuro: false
-      }
-    },
-    computed: {
-      textoBotao() {
-        if(this.modoEscuro){
-          return 'Desativar modo escuro'
-        }
-        return 'Ativar modo escuro'
-      }
-    },
-    methods: {
-      alterarTema(){
-        this.modoEscuro = !this.modoEscuro
-        this.$emit('alterarTema', this.modoEscuro)
-      }
+  name: 'BarraLateral',
+  emits: ['alterarTema'],
+  data() {
+    return {
+      modoEscuro: false
     }
+  },
+  computed: {
+    textoBotao() {
+      if (this.modoEscuro) {
+        return 'Desativar modo escuro'
+      }
+      return 'Ativar modo escuro'
+    }
+  },
+  methods: {
+    alterarTema() {
+      this.modoEscuro = !this.modoEscuro
+      this.$emit('alterarTema', this.modoEscuro)
+    }
+  }
 });
 
 </script>
@@ -63,22 +63,26 @@ header {
   height: 100vh;
   text-align: center;
 }
+
 @media only screen and (max-width: 768px) {
   header {
     padding: 2.5rem;
     height: auto;
   }
 }
+
 .panel li {
-    margin: 8px 0;
+  margin: 8px 0;
 }
+
 .link {
-    color: #fff;
+  color: #fff;
 }
+
 .link:hover {
-    color: #FAF0CA;
+  color: #FAF0CA;
 }
+
 .link.router-link-active {
-    color: #FAF0CA;
-}
-</style>
+  color: #FAF0CA;
+}</style>
